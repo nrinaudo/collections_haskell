57f6d2eeb95bfa8b73b1e2d98b4640d6b81047f4
nrinaudo <nicolas@nrinaudo.com>
2014-08-26 22:27:13 +0200

    Removed unnecessary Ord type constraints.

--- src/test/Data/Collection/SetTests.hs
+++ src/test/Data/Collection/SetTests.hs
@@ -7,22 +7,22 @@ import Test.QuickCheck.Property
 import Test.QuickCheck
 
 -- Creates tests for the specified set implementation.
-setTests :: (Set s, Ord a, Arbitrary a, Show a) => s a -> String -> Test
+setTests :: (Set s, Arbitrary a, Show a) => s a -> String -> Test
 setTests s label = testGroup label [
   testProperty "the empty set should be empty"             (prop_emptyIsEmpty s),
   testProperty "the empty should not contain any element"  (prop_emptyContainsNothing s),
   testProperty "a set should contains all of its elements" (prop_containsContent s)
   ]
 
-createSet :: (Set s, Ord a) => s a -> [a] -> s a
+createSet :: (Set s) => s a -> [a] -> s a
 createSet s as = foldl insert s as
 
-prop_emptyContainsNothing :: (Set s, Ord a) => s a -> a -> Bool
+prop_emptyContainsNothing :: (Set s) => s a -> a -> Bool
 prop_emptyContainsNothing s = not . contains s
 
-prop_emptyIsEmpty :: (Set s, Ord a) => s a -> Bool
+prop_emptyIsEmpty :: (Set s) => s a -> Bool
 prop_emptyIsEmpty = isEmpty
 
-prop_containsContent :: (Set s, Ord a) => s a -> [a] -> Bool
+prop_containsContent :: (Set s) => s a -> [a] -> Bool
 prop_containsContent s as = all (contains set) as
                             where set = createSet s as
